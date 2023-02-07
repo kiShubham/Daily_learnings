1ace857ae75ed0e44f06dcd42126cb37d56220d2
RocK-Y Lee <shubhamshubhank1997@gmail.com>
2023-02-07 12:14:32 +0000

    somechanges

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 // console.log("Hello CodeSandbox");
 // console.log("hello");
 let a = { a: 6 };
@@ -10,13 +8,10 @@ let b = { a: 7 };
 // }
 // console.log(av(a, b));
 
-
-// apply method . 
+// apply method .
 let num = [90, 100];
 console.log(Math.max.apply(null, num)); //100
 console.log(Math.min.apply(null, num)); //90;
-
-
 
 // console.log("Hello CodeSandbox");
 
@@ -43,18 +38,18 @@ getConfig is a method :A method is a function which is a property of an object.
 */
 
 let laptop2 = {
-    cpu: "i8",
-    ram: 8,
-    brand: "hp",
-    getConfig: function () {
-      console.log(this.cpu);
-    },
-  };
-  laptop2.getConfig();
-  
-  //now lets compare laptop1 with laptop2
-  //creating a compare function ;
-  /*
+  cpu: "i8",
+  ram: 8,
+  brand: "hp",
+  getConfig: function () {
+    console.log(this.cpu);
+  },
+};
+laptop2.getConfig();
+
+//now lets compare laptop1 with laptop2
+//creating a compare function ;
+/*
   let compare = (laptop1,laptop2) => {
       if(laptop1.cpu > laptop2.cpu)
           console.log("laptop1")
@@ -62,24 +57,50 @@ let laptop2 = {
   }
   compare(laptop1,laptop2);//laptop2;
   */
-  
-  // now creating a property compare inside the Object;
-  let laptop1 = {
-    cpu: "i5",
-    ram: 8,
-    brand: "dell",
-  
-    compare: function (other) {
-      //other is just a parameter not a keyword like this.  ;
-      if (this.cpu > other.cpu) console.log(this);
-      else console.log(other);
-    },
-  
-    getConfig: function () {
-      console.log(this.cpu);
-    },
-  };
-  
-  //calling the function ,as it is property of laptop1;
-  laptop1.compare(laptop2); //laptop2{};
-  
+
+// now creating a property compare inside the Object;
+let laptop1 = {
+  cpu: "i5",
+  ram: 8,
+  brand: "dell",
+
+  compare: function (other) {
+    //other is just a parameter not a keyword like this.  ;
+    if (this.cpu > other.cpu) console.log(this);
+    else console.log(other);
+  },
+
+  getConfig: function () {
+    console.log(this.cpu);
+  },
+};
+
+//calling the function ,as it is property of laptop1;
+laptop1.compare(laptop2); //laptop2{};
+
+// ---------------------classes --------------------------------------------
+
+//first learn constructor function
+
+//this is a constructor function;
+function Alien(name, tech) {
+  this.name = name;
+  this.tech = tech;
+  // return this ;isko likhne ki jarurat nhi h cuz ye ek constructor function h ,default h ;
+}
+
+let alien1 = new Alien("hauser", "pegasus");
+//the " new " keyword create new objects every time ;
+let alien2 = new Alien("show", "java");
+let alien3 = new Alien("dakota", "rocky");
+let alien4 = Alien("akota", "rocky");
+
+console.log(alien1);
+//Alien { name: 'hauser', tech: 'pegasus' }
+console.log(alien2);
+//Alien { name: 'show', tech: 'java' }
+console.log(alien3);
+//Alien { name: 'dakota', tech: 'rocky' }
+console.log(alien4); // undefined
+
+//see everytime a new object from same function ; thus it is called a constructor function;
