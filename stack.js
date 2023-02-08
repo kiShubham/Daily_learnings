91595ec345b55c9a178fbc15f029c040301efe3e
RocK-Y Lee <shubhamshubhank1997@gmail.com>
2023-02-08 14:08:29 +0000

    questions

--- stack.js
+++ stack.js
@@ -59,93 +59,198 @@ console.log(mystack); // Stacks { arr: [ 5, 4, 8 ] }
 
 //----------- stacks using linked list --------------------
 
-//to create new node
-class Node {
-  constructor(data) {
-    this.value = data; //
-    this.next = null; //**very imp -here null is claryfying that this is a node */
-  }
-}
-
-class Stacks {
-  constructor() {
-    this.top = null; // node
-    this.length = 0;
-  }
-  //when we had to add new element in the stack using linkedlist we will create new node ;either we can write in this class or we can write in separate class ,make seprate class Node;
-
-  append(value) {
-    let myNode = new Node(value);
-    //now before appending check elements in linkedlist
-    // 1.if linkedlist is empty
-    if (this.length == 0) {
-      this.top = myNode;
-    }
-    // 2.linkedlist is not empty;
-    else {
-      let holdingpointer = this.top; //holdingpointer become null
-      this.top = myNode; // this.top become the myNode means the value;
-      myNode.next = holdingpointer; // mynode.next means previous value of top become (holdingpointer)i.e, null ;
-      //therefore,     null <--- top
-    }
-    this.length++; //updating the lenth of linked list ;
-  }
-
-  display() {
-    return this.top;
-  }
-
-  // now to the element from the (stack) linked list ,another fn to delete last element or top element;
-
-  // Null <-- 1 <-- 2 <-- 3; so here to delete '3',we have to put the top pointer on '2' and automatically the "3" would become a garbage value ;
-
-  delete() {
-    let newTop = this.top.next; // this.top = 3,this.top.next = 2;so now modify value of this.top to 2 ;
-    this.top = newTop; //now  this.top = 2
-
-    this.length--; //updating length of linkedlist;
-  }
-  /*
-  pop() {
-    if (!this.top) {
-      return null;
-    }
-    if (this.top === this.bottom) {//for this make this.bottom property inside constructor;
-      this.bottom = null;
-    }
-
-    const newTop = this.top.next;
-    this.top = newTop;
-    this.length--;
-    return this;
-  }
-  */
-}
-
-let mystack = new Stacks();
-//console.log(mystack);// Stacks { top: null, length: 0 }
-mystack.append(1);
-mystack.append(2);
-mystack.append(3);
-console.log(mystack.display());
 /*
-Node { value: 3,
-     next: Node { value: 2, 
-                    next: Node { value: 1,
-                                 next: null } 
-                }  
+  //to create new node
+  class Node {
+    constructor(data) {
+      this.value = data; //
+      this.next = null; 
+      //very imp -here null is claryfying that this is a node 
     }
-raw => 1 --> 2 --> 3 --> Null;
-    */
-console.log(mystack.length); // 3
+  }
 
-mystack.delete(); //( Null <-- 1 <-- 2 <-- 3 ) to ( Null <-- 1 <-- 2 )
-console.log(mystack.length); // 2
-console.log(mystack.display()); // ( Null <-- 1 <-- 2 )
-/*
-Node { value: 2, 
-        next: Node { value: 1, 
-                        next: null 
-                        } 
-    }  
+  class Stacks {
+    constructor() {
+      this.top = null; // node
+      this.length = 0;
+    }
+    //when we had to add new element in the stack using linkedlist we will create new node ;either we can write in this class or we can write in separate class ,make seprate class Node;
+
+    append(value) {
+      let myNode = new Node(value);
+      //now before appending check elements in linkedlist
+      // 1.if linkedlist is empty
+      if (this.length == 0) {
+        this.top = myNode;
+      }
+      // 2.linkedlist is not empty;
+      else {
+        let holdingpointer = this.top; //holdingpointer become null
+        this.top = myNode; // this.top become the myNode means the value;
+        myNode.next = holdingpointer; // mynode.next means previous value of top become (holdingpointer)i.e, null ;
+        //therefore,     null <--- top
+      }
+      this.length++; //updating the lenth of linked list ;
+    }
+
+    display() {
+      return this.top;
+    }
+
+    // now to the element from the (stack) linked list ,another fn to delete last element or top element;
+
+    // Null <-- 1 <-- 2 <-- 3; so here to delete '3',we have to put the top pointer on '2' and automatically the "3" would become a garbage value ;
+
+    delete() {
+      let newTop = this.top.next; // this.top = 3,this.top.next = 2;so now modify value of this.top to 2 ;
+      this.top = newTop; //now  this.top = 2
+
+      this.length--; //updating length of linkedlist;
+    }
+    
+      // pop() {
+      //   if (!this.top) {
+      //     return null;
+      //   }
+      //   if (this.top === this.bottom) {//for this make this.bottom property inside constructor;
+      //     this.bottom = null;
+      //   }
+
+      //   const newTop = this.top.next;
+      //   this.top = newTop;
+      //   this.length--;
+      //   return this;
+      // }
+    
+  }
+
+
+  let mystack = new Stacks();
+  //console.log(mystack);// Stacks { top: null, length: 0 }
+  mystack.append(1);
+  mystack.append(2);
+  mystack.append(3);
+  console.log(mystack.display());
+  
+  // Node { value: 3,
+  //     next: Node { value: 2, 
+  //                     next: Node { value: 1,
+  //                                 next: null } 
+  //                 }  
+  //     }
+  // raw => 1 --> 2 --> 3 --> Null;
+      
+     
+  console.log(mystack.length); // 3
+
+  mystack.delete(); //( Null <-- 1 <-- 2 <-- 3 ) to ( Null <-- 1 <-- 2 )
+  console.log(mystack.length); // 2
+  console.log(mystack.display()); // ( Null <-- 1 <-- 2 )
+  
+  // Node { value: 2, 
+  //         next: Node { value: 1, 
+  //                         next: null 
+  //                         } 
+  //     }  
+
 */
+
+// questions
+// let arr = [1, 3, 2, 4]
+// ans =  [3, 4, 4,-1]
+// let arr =[1,3,0,0,1,2,4]
+//ans = [3,4,1,1,2,4,-1];
+let arr= [3,4,4,1];
+// console.log(arr.reverse())
+// let ans = [] ;
+// console.log(!ans[ans.length -1]);
+
+/*
+function nextLargerElement(n, arr) {
+
+
+  let stack = [] ;//stack.top is stack[stack.length - 1]
+  let ans = [] ;
+ 
+  for( var i = n-1; i >= 0; i-- ){
+   //stack contain element right side of arr[i];as asked in Qs;
+    stack.push( arr[i + 1]);
+    if(stack[stack.length - 1] == undefined) stack.pop();
+    console.log(arr[i] , stack);
+ 
+    if( stack.length == 0) {//if  stack is empty;
+      ans.push( -1 )
+    }
+    else if( stack[stack.length - 1] > arr[i] ){
+      ans.push(stack[stack.length - 1]) //if stack top value is greater than arr[i];
+    }
+    else {
+      //
+      while( stack.length !== 0 ){ 
+
+        if(arr[i] < stack[stack.length-1] ) {
+          ans.push(stack[stack.length - 1]) 
+          break;// && arr[i] >= stack[stack.length -1] 
+        }
+        else if( arr[i] >= stack[stack.length - 1] ){
+          stack.pop();
+        }
+        else if(stack.length == 0) {
+          ans.push(-1)
+        }
+        }
+    }
+  }
+  // return 0;
+  return ans.reverse();
+}
+console.log(nextLargerElement(4,arr));
+*/
+
+function nextLargerElement(n, arr) {
+
+
+  let stack = [] ;//stack.top is stack[stack.length - 1]
+  let k = stack.length;
+  let ans = [] ;
+ for(var i = n-1; i >= 0 ; i--){
+  
+  if(k == 0){
+    ans.push(-1);
+  }
+  else if( stack[k - 1] > arr[i] ){
+    ans.push(stack[k - 1]) 
+  }
+  else if( stack[k - 1] < arr[i] ){
+
+    while( k > 0 && stack[k -1 ] <= arr[i] ){
+      stack.pop();
+    }
+    if(k == 0) ans.push(-1)
+    else if( stack[k - 1] > arr[i] ) ans.push(stack[k-1]);
+  }
+  // console.log(arr[i], stack);
+  stack.push( arr[i] );
+  
+ }
+ ans.reverse();
+}
+console.log(nextLargerElement(4,arr));
+
+
+// class Stack{
+//   constructor(){
+//     this.arr = [] ;    
+//     this.top = arr[arr.length-1] ; 
+//   }
+//   add(){
+//     if(this.arr.length = 0){
+//       return ans.push(-1)
+//     }else if( ) 
+//   }
+//   display(){
+     
+//   }
+// }
+
+
